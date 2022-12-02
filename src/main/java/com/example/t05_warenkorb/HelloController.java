5cee45d4ba4c503addc2298492160747eb77cb01
cortenv <vera.corten@hotmail.com>
2022-12-02 15:27:03 +0100

    Initial commit

---
+++ src/main/java/com/example/t05_warenkorb/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.t05_warenkorb;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class HelloController {
+
+    @FXML
+    private TextField textFieldNumber;
+
+    @FXML
+    private Hyperlink hyperlink;
+
+    @FXML
+    private TextField textFieldProduct;
+
+    @FXML
+    private MenuButton menuButton;
+
+    @FXML
+    private Button addToCart;
+
+
+}
