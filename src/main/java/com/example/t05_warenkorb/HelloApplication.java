ecbe6a812a9a1db70f577c173d5157c25f3c9362
cortenv <vera.corten@hotmail.com>
2022-12-07 13:15:59 +0100

    final

--- src/main/java/com/example/t05_warenkorb/HelloApplication.java
+++ src/main/java/com/example/t05_warenkorb/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Bestellung erfassen");
+        Scene scene = new Scene(fxmlLoader.load(), 620, 400);
+        stage.setTitle("Warenkorb");
         stage.setScene(scene);
         stage.show();
     }
